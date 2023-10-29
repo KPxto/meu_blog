ddb67b87f4e54826537ca5257b86b05509fce647
Kpxto <kaioopeixoto@gmail.com>
2023-10-29 17:24:25 -0300

    teste3

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-'kaio'
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
